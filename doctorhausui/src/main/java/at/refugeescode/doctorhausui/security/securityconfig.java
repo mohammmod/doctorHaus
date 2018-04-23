6872e7e51bb1301e52a6dcbca0870983a7fe97c7
mohammmod <mohammad.sawas2@outlook.com>
2018-04-23 18:54:39 +0200

    adding security

---
+++ doctorhausui/src/main/java/at/refugeescode/doctorhausui/security/securityconfig.java
@@ -0,0 +1,51 @@
+package at.refugeescode.doctorhausui.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+public class securityconfig extends WebSecurityConfigurerAdapter{
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/inserPaitients").permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> users = Stream.of(
+                User.withUsername("user").password(encoder.encode("user")).roles("USER").build()
+
+        ).collect(Collectors.toList());
+        return new InMemoryUserDetailsManager(users);
+    }
+
+
+}
