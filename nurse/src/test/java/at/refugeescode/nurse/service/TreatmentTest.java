ca0d7d7801a057b8d403f92fc4a074fb6e908c66
mohammmod <mohammad.sawas2@outlook.com>
2018-05-21 01:07:06 -0930

    Adding tested

--- nurse/src/test/java/at/refugeescode/nurse/service/TreatmentTest.java
+++ nurse/src/test/java/at/refugeescode/nurse/service/TreatmentTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreatmentTest {
-    Treatment treat = new Treatment();
+    Treatment treatment = new Treatment();
     @Test
     void getTreatment() {
-        String trea = treat.getTreatment("cold");
-        assertEquals("drinkWater", trea);
+        String treat = treatment.getTreatment("cold");
+        assertEquals("drinkWater", treat);
     }
 }
